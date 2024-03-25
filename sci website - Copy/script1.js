0d4e11cac5ea7a862145e577c8992c996347224b
Baruska <164750314+baruskaa@users.noreply.github.com>
2024-03-25 16:40:59 +0800

    Add files via upload

---
+++ sci website - Copy/script1.js
@@ -0,0 +1,13 @@
+const trigger = document.querySelector(".trigger");
+const wrapper = document.querySelector(".wrapper");
+
+function handleTrigger() {
+  wrapper.classList.remove("hide");
+}
+
+function handleWrapper() {
+  wrapper.classList.add("hide");
+}
+
+trigger.addEventListener("mouseenter", handleTrigger);
+wrapper.addEventListener("click", handleWrapper);
